9a214083632ade34e7a5d3e2e75510ca69c15a94
FWasik <franekwasik2@gmail.com>
2025-02-05 19:29:43 +0100

    feat: models with serializers and views

--- hydro_sys/systems/models.py
+++ hydro_sys/systems/models.py
@@ -1,1 +1,35 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class HydroponicSystem(models.Model):
+    types = [
+        ("NFT", "Nutrient Film Technique"),
+        ("DWC", "Deep Water Culture"),
+        ("Ebb and Flow", "Ebb and Flow"),
+        ("Aeroponics", "Aeroponics"),
+        ("Drip", "Drip System"),
+        ("Wick", "Wick System"),
+    ]
+
+    name = models.CharField(max_length=125, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="systems")
+    type = models.CharField(max_length=50, choices=types)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    system = models.ForeignKey(
+        HydroponicSystem, on_delete=models.CASCADE, related_name="measurements"
+    )
+    ph = models.FloatField(help_text="In the pH scale (0-14)")
+    temperature = models.FloatField(help_text="In Celcius (°C)")
+    tds = models.FloatField(help_text="Parts per milion (ppm)")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
--- hydro_sys/systems/views.py
+++ hydro_sys/systems/views.py
@@ -1,1 +1,44 @@
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from .models import HydroponicSystem, Measurement
+from .serializers import HydroponicSystemSerializer, MeasurementSerializer
 
+
+class HydroponicSystemViewSet(viewsets.ModelViewSet):
+    serializer_class = HydroponicSystemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return HydroponicSystem.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.owner != request.user:
+            return Response(
+                {"error": "You are not allowed to delete this system"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        return super().destroy(request, *args, **kwargs)
+
+
+class MeasurementViewSet(viewsets.ModelViewSet):
+    serializer_class = MeasurementSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Measurement.objects.filter(system__owner=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.system.owner != request.user:
+            return Response(
+                {"error": "You are not allowed to delete this measurement"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        return super().destroy(request, *args, **kwargs)
