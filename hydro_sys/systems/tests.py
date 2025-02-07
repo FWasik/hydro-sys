b9b42ce400f82b40115403770769a7d8b6facbf8
FWasik <franekwasik2@gmail.com>
2025-02-07 12:02:56 +0100

    hotfix: phone numbers for users in tests

--- hydro_sys/systems/tests.py
+++ hydro_sys/systems/tests.py
@@ -10,10 +10,16 @@ User = get_user_model()
 class BaseTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(
-            username="test_user", email="test_email@email.com", password="test_pass"
+            username="test_user",
+            email="test_email@email.com",
+            password="test_pass",
+            phone_number="123456789",
         )
         self.other_user = User.objects.create_user(
-            username="other_user", email="other_email@email.com", password="test_pass"
+            username="other_user",
+            email="other_email@email.com",
+            password="test_pass",
+            phone_number="987654321",
         )
 
         self.client.force_authenticate(user=self.user)
